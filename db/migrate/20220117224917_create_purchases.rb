7af77ab73a03437dfeddad318f5f9a9bd96a89c4
codecaiine <akabrouyannickn@gmail.com>
2022-01-17 22:53:54 +0000

    Modify migration for group and purchase

--- db/migrate/20220117224917_create_purchases.rb
+++ db/migrate/20220117224917_create_purchases.rb
@@ -1,6 +1,9 @@
 class CreatePurchases < ActiveRecord::Migration[6.1]
   def change
     create_table :purchases do |t|
+      t.string :name
+      t.float :amount
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
