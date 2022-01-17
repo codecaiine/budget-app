e404268ef0d35346b386b09ff33358ab15e53524
codecaiine <akabrouyannickn@gmail.com>
2022-01-17 22:59:11 +0000

    Add association purchase and group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+    has_and_belongs_to_many :purchases
+    
+    belongs_to :user
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,5 @@
 class Purchase < ApplicationRecord
+    has_and_belongs_to_many :groups
+    
+    belongs_to :user
 end
