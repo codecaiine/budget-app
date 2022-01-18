dbb086ffd583fe8eed602bd18fdec52b5a130c24
codecaiine <akabrouyannickn@gmail.com>
2022-01-18 00:14:45 +0000

    Add migration

--- db/migrate/20220117230728_create_groups_purchases.rb
+++ db/migrate/20220117230728_create_groups_purchases.rb
@@ -1,12 +1,10 @@
 class CreateGroupsPurchases < ActiveRecord::Migration[6.1]
   def change
-    create_table :group_purchases do |t|
+    create_table :groups_purchases do |t|
       t.references :group
       t.references :purchase
+
+      t.timestamps
     end
-    
-    add_index :groups_purchases, [:group_id, :purchase_id],
-      name: "groups_purchases_index",
-      unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_194733) do
+ActiveRecord::Schema.define(version: 2022_01_18_001214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_groups_on_author_id"
+  end
+
+  create_table "groups_purchases", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "purchase_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_groups_purchases_on_group_id"
+    t.index ["purchase_id"], name: "index_groups_purchases_on_purchase_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_purchases_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +58,6 @@ ActiveRecord::Schema.define(version: 2022_01_17_194733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "purchases", "users", column: "author_id"
 end
