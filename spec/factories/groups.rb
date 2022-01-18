dc40efbfe99dcdfdbac7ba9ff40d6925f0202189
codecaiine <akabrouyannickn@gmail.com>
2022-01-18 19:24:09 +0000

    Generate my test model and controller

---
+++ spec/factories/groups.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :groups do
+    
+  end
+end
---
+++ spec/models/purchases_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Purchases, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
