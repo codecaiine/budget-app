0eecfe07367637707c68e0dcbe93637c23cd262f
codecaiine <akabrouyannickn@gmail.com>
2022-01-18 12:43:32 +0000

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resources)
+  def after_sign_in_path_for(_resources)
     groups_path
   end
 
-  def after_sign_up_path_for(resources)
+  def after_sign_up_path_for(_resources)
     groups_path
   end
 
