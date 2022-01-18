3315aff6c068ad2ce8604e705cea64ebdabca441
codecaiine <akabrouyannickn@gmail.com>
2022-01-18 00:18:03 +0000

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :purchases
-  resources :groups
   devise_for :users
-  resources :users
   root 'users#main_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
