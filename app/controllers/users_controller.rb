bb85e4ee802d5f2a99746bee930c60e96347bf30
codecaiine <akabrouyannickn@gmail.com>
2022-01-20 15:49:38 +0000

    Update users controller add main_page method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,29 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  def main_page; end
+  def main_page
+    @current_user = current_user
+    authorize! :read, User
+  end
 
   # GET /users or /users.json
   def index
     @users = User.all
   end
-
   # GET /users/1 or /users/1.json
   def show; end
-
   # GET /users/new
   def new
     @user = User.new
   end
-
   # GET /users/1/edit
   def edit; end
-
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -33,12 +31,11 @@ class UsersController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -46,26 +43,21 @@ class UsersController < ApplicationController
       end
     end
   end
-
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
-
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
-
   private
-
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
-
   # Only allow a list of trusted parameters through.
   def user_params
-    params.fetch(:user, {})
+    params.require(:user).permit(:name)
   end
 end
